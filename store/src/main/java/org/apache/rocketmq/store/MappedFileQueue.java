0cd23990fc68f23535bd8583358da19f4a2c13e3
qinpr <peirongq_1@tujia.com>
2019-02-14 17:33:21 +0800

    cm

--- store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
+++ store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
@@ -34,16 +34,17 @@ public class MappedFileQueue {
     private static final InternalLogger LOG_ERROR = InternalLoggerFactory.getLogger(LoggerName.STORE_ERROR_LOGGER_NAME);
 
     private static final int DELETE_FILES_BATCH_MAX = 10;
-
+    //存储路劲
     private final String storePath;
-
+    //单个文件的存储大小
     private final int mappedFileSize;
-
+    //MappedFile文件集合
     private final CopyOnWriteArrayList<MappedFile> mappedFiles = new CopyOnWriteArrayList<MappedFile>();
-
+    //创建MappedFile服务类
     private final AllocateMappedFileService allocateMappedFileService;
-
+    //当前刷盘指针，表示该指针之前的所有数据全部持久化到磁盘
     private long flushedWhere = 0;
+    //当前数据提交指针，内存中ByteBuffer当前的写指针，该值大于等于flushedWhere
     private long committedWhere = 0;
 
     private volatile long storeTimestamp = 0;
@@ -74,6 +75,13 @@ public class MappedFileQueue {
         }
     }
 
+    /**
+     * 根据消息存储时间戳来查找MappdFile
+     * 从MappdFile列表中第一个文件开始查找，找到第一个最后一次更新时间大约待查找时间戳的文件
+     * 如果不存在，则返回最后一个MappdFile文件
+     * @param timestamp
+     * @return
+     */
     public MappedFile getMappedFileByTime(final long timestamp) {
         Object[] mfs = this.copyMappedFiles(0);
 
